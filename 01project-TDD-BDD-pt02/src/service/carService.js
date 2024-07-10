8269f2aee6cb3eb24b1fcfaf9f81a0d4350ccba8
Cleo Silva <cleo_silva1906@hotmail.com>
2024-07-10 19:40:33 -0300

    Replacing files

---
+++ 01project-TDD-BDD-pt02/src/service/carService.js
@@ -0,0 +1,12 @@
+const BaseRepository = require('./../repository/base/baseRepository');
+class CarService {
+    constructor({ cars }) {
+        this.carRepository = new BaseRepository({ file: cars })
+    }
+
+    test() {
+        return this.carRepository.find()
+    }
+
+}
+module.exports = CarService;
