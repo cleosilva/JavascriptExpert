d2c208ac3df5246855489bb4de26b49b0fbf9f54
Cleo Silva <cleo_silva1906@hotmail.com>
2024-07-12 18:15:54 -0300

    Creating tests that search an available car

--- 01project-TDD-BDD-pt02/src/service/carService.js
+++ 01project-TDD-BDD-pt02/src/service/carService.js
@@ -4,8 +4,25 @@ class CarService {
         this.carRepository = new BaseRepository({ file: cars })
     }
 
-    test() {
-        return this.carRepository.find()
+    getRandomPositionFromArray(list) {
+        const listLength = list.length
+        return Math.floor(Math.random() * (listLength))
+
+    }
+    chooseRandomCar(carCategory) {
+        const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds);
+        console.log('randomCarIndex', randomCarIndex)
+        const carId = carCategory.carIds[randomCarIndex];
+
+        console.log('carId chooseRandom', carId)
+        return carId;
+    }
+    async getAvailableCar(carCategory) {
+        const carId = this.chooseRandomCar(carCategory);
+        console.log('CarID getAvailable', carId)
+        const car = await this.carRepository.find(carId)
+        console.log('car getAvailable', car)
+        return car;
     }
 
 }
